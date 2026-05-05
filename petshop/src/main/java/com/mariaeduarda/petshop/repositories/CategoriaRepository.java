9115a0bc557222cc92a991a4a6e14de9211565e2
dudinha015 <maria.arruda1a@gmail.com>
2026-05-05 15:37:38 -0300

    Delete petshop directory

--- petshop/src/main/java/com/mariaeduarda/petshop/repositories/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mariaeduarda.petshop.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mariaeduarda.petshop.entities.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
--- petshop/src/main/java/com/mariaeduarda/petshop/repositories/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mariaeduarda.petshop.repositories;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mariaeduarda.petshop.entities.Produto;
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-    List<Produto> findByCategoriaIdAndAtivoTrue(Integer idCategoria);
-}
